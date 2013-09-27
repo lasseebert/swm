ff5a0fc6d4086130efb202d3f20a779725111250
Lasse Skindstad Ebert <lasseebert@gmail.com>
2013-09-27 23:58:56 +0200

    Failing spec

--- spec/unit/commands/resize_command_spec.rb
+++ spec/unit/commands/resize_command_spec.rb
@@ -3,8 +3,9 @@ require 'spec_helper'
 describe Swm::ResizeCommand do
   it 'should preserve window position when resizing with empty arguments' do
     resize_command = Swm::ResizeCommand.new
-    window = Swm::Window.current
+    window_id = Swm::Window.current.id
     expect { resize_command.run }.to_not change {
+      window = Swm::Window.new window_id
       [
         window.pos_x,
         window.pos_y,
