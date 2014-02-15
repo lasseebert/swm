48e1f9cd7f273ebf8c96c990d76206505258a34c
Lasse Skindstad Ebert <lasseebert@gmail.com>
2014-02-15 19:24:54 +0100

    Can now use margins in resize command

--- lib/swm/commands/resize_command.rb
+++ lib/swm/commands/resize_command.rb
@@ -15,6 +15,10 @@ Options:
   --y:      Y-ccord of top-left corner in percent of screen
   --width:  Width in percent of screen
   --height: Height in percent of screen
+  --left:   (Optional) Sets a left margin of the screen
+  --right:  (Optional) Sets a right margin of the screen
+  --top:    (Optional) Sets a top margin of the screen
+  --bottom: (Optional) Sets a bottom margin of the screen
 Examples:
   Make window fill 80 percent of screen in each dimension with a 10 percent margin:
     swm resize --x 10 --y 10 --width 80 --height 80
@@ -28,20 +32,35 @@ Examples:
     end
 
     def run
+      screen_dimensions = Screen.dimensions
+      rect = [0, 0, screen_dimensions[0], screen_dimensions[1]]
+      set_offset(rect)
+      resize_in_rect(rect)
+    end
+
+    private
+
+    def set_offset(rect)
+      rect[0] += options[:left] || 0
+      rect[2] -= options[:left] || 0
+      rect[1] += options[:top] || 0
+      rect[3] -= options[:top] || 0
+      rect[2] -= options[:right] || 0
+      rect[3] -= options[:bottom] || 0
+    end
+
+    def resize_in_rect(rect)
       x_percent = options[:x]
       y_percent = options[:y]
       width_percent = options[:width]
       height_percent = options[:height]
 
-      screen_dimensions = Screen.dimensions
-      window = Swm::Window.current
+      x =       (rect[0] + rect[2] * x_percent / 100.0).to_i
+      y =       (rect[1] + rect[3] * y_percent / 100.0).to_i
+      width =   (rect[2] * width_percent / 100.0).to_i
+      height =  (rect[3] * height_percent / 100.0).to_i
 
-      x = y = width = height = nil
-      x =       (screen_dimensions[0] * x_percent / 100.0).to_i
-      y =       (screen_dimensions[1] * y_percent / 100.0).to_i
-      width =   (screen_dimensions[0] * width_percent / 100.0).to_i
-      height =  (screen_dimensions[1] * height_percent / 100.0).to_i
-
+      window = Window.current
       window.set x, y, width, height
     end
 
